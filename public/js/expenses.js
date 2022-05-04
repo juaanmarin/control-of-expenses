5455bb800b64b8a554f6174a1f98e2573ae7fc68
juan david marin <juaanmarin2007@gmail.com>
2022-05-04 09:10:17 -0500

    se agrego las interfaces, clases y metodos para poder controlar los gastos

---
+++ public/js/expenses.js
@@ -0,0 +1,80 @@
+"use strict";
+// clase con dato personalizado de tipo arraylist
+class ArrayList {
+    constructor() {
+        this.items = [];
+    }
+    add(item) {
+        this.items.push(item);
+    }
+    get(index) {
+        const item = this.items.filter((x, i) => {
+            return i === index;
+        });
+        if (item.length === 0) {
+            return null;
+        }
+        else {
+            return item[0];
+        }
+    }
+    createFerom(value) {
+        this.items = [...value];
+    }
+    getAll() {
+        return this.items;
+    }
+}
+//clase que enplementa todo los metodos de la interfase iexpnses
+class Expenses {
+    constructor(currency) {
+        this.count = 0;
+        this.finalCurrency = currency = currency;
+        this.expenses = new ArrayList();
+    }
+    add(item) {
+        item.id = this.count;
+        this.count++;
+        this.expenses.add(item);
+        return true;
+    }
+    get(index) {
+        return this.expenses.get(index);
+    }
+    getItems() {
+        return this.expenses.getAll();
+    }
+    getTotal() {
+        const total = this.expenses.getAll().reduce((acc, item) => {
+            return acc += this.convertCurrency(item, this.finalCurrency);
+        }, 0);
+        return `${this.finalCurrency} ${total.toFixed(2).toString}`;
+    }
+    remove(id) {
+        throw new Error("Method not implemented.");
+    }
+    convertCurrency(item, currency) {
+        switch (item.cost.currency) {
+            case "USD":
+                switch (currency) {
+                    case "MXN":
+                        return item.cost.number * 22;
+                        break;
+                    default:
+                        return item.cost.number;
+                }
+                break;
+            case "MXN":
+                switch (currency) {
+                    case "USD":
+                        return item.cost.number / 22;
+                        break;
+                    default:
+                        return item.cost.number;
+                }
+                break;
+            default:
+                return 0;
+        }
+    }
+}
--- src/expenses.ts
+++ src/expenses.ts
@@ -1,17 +1,116 @@
-type Currency = "MXN" | "USD" | "COP";
+type Currency = "MXN" | "USD" | "COP"; //definimos que currency no puede tener otro tipo de dato que nos sea("MXN","USD","COP")
 
+//interfas personalizada que espera dos datos uno primitivo y uno personalizado
 interface Price{
-    number:number;
+    number:number,
     currency:Currency
 }
 
+//ExpensesItems espera tres datos dos de tipo primitibo y uno personalisado
 interface ExpenseItems{
-    id:number;
-    title:string;
-    cost:Price;
+    id:number,
+    title:string,
+    cost:Price
 }
 
-class Expenses {
-    // constructor(parameters) {  
-    // }
+//interfas en la que se inplemanta un dato personalizado de tipo Arraylist
+interface iExpenses{
+    expenses:ArrayList<ExpenseItems>,
+    finalCurrency: Currency,
+    add(item:ExpenseItems):boolean,
+    get(index:number):ExpenseItems|null,
+    getTotal():string,
+    remove(id:number):boolean
+}
+
+// clase con dato personalizado de tipo arraylist
+class ArrayList<T>{
+    private items:T[];
+
+    constructor (){
+        this.items=[];
+    }
+    add(item:T):void{
+        this.items.push(item);
+    }
+
+    get(index:number):T|null{
+        const item:T[]=this.items.filter((x,i:number)=>{
+            return i === index;
+        });
+        if(item.length===0){
+            return null;
+        }
+        else{
+            return item[0];
+        }
+    }
+    createFerom(value:T[]):void{
+        this.items=[...value];
+    }
+    getAll():T[]{
+        return this.items;
+    }
+}
+
+//clase que enplementa todo los metodos de la interfase iexpnses
+class Expenses implements iExpenses{
+
+    expenses: ArrayList<ExpenseItems>;
+    finalCurrency: Currency;
+
+    private count=0;
+    
+    constructor(currency:Currency) {  
+        this.finalCurrency=currency=currency;
+        this.expenses=new ArrayList<ExpenseItems>();
+    }
+
+    add(item: ExpenseItems): boolean {
+        item.id=this.count;
+        this.count++;
+        this.expenses.add(item);
+        return true;
+    }
+    get(index:number): ExpenseItems | null {
+        return this.expenses.get(index);
+    }
+    getItems():ExpenseItems[]{
+        return this.expenses.getAll();
+    }
+    getTotal(): string {
+        const total=this.expenses.getAll().reduce((acc,item)=>{
+            return acc+= this.convertCurrency(item, this.finalCurrency);
+        },0); 
+        
+        return `${this.finalCurrency} ${total.toFixed(2).toString}`;
+    } 
+    remove(id: number): boolean {
+        throw new Error("Method not implemented.");
+    }
+    private convertCurrency(item:ExpenseItems, currency:Currency){
+        switch(item.cost.currency){
+            case "USD":
+                switch(currency){
+                    case "MXN":
+                        return item.cost.number*22;
+                    break;
+                    default:
+                        return item.cost.number;
+                }
+            break;
+            case "MXN":
+                switch(currency){
+                    case "USD":
+                        return item.cost.number/22;
+                    break;
+                    default:
+                        return item.cost.number;
+                }
+            break;
+
+            default:
+                return 0;
+        }
+    }
 }
